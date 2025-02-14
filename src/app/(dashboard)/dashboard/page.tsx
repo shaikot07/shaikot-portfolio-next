7e4a74409927dceaea2e2eff6009b87082be5cf1
shaikot07 <saifulislamshaikat007@gmail.com>
2025-02-14 22:41:57 +0600

    updated the admin home page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 
 
-const page = () => {
+const AdminHomepage = () => {
     return (
         <div>
-            this si the dashboard page only for admin not for users
+            <h2 className="text-center text-red-800 mt-10">this si the dashboard page only for admin not for users</h2>
         </div>
     );
 };
 
-export default page;
+export default AdminHomepage;
